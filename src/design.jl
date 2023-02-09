50ed4f4e4d6f04cf1523122c6362694c43fc1453
Patrick Leibersperger <pleibersperger@posteo.de>
2023-02-09 14:06:48 +0100

    adjust optional kwargs

--- src/design.jl
+++ src/design.jl
@@ -1,16 +1,16 @@
-function set_design(themesym::Symbol; font="Palatino", dpi=300, tickfontsize=12, kwargs...)
+function set_design(themesym::Symbol; font="Palatino", dpi=300, tickfontsize=12, plot=())
     theme(themesym)
-    default(fontfamily=font, dpi=dpi, tickfontsize=tickfontsize, kwargs...)
+    default(fontfamily=font, dpi=dpi, tickfontsize=tickfontsize, plot...)
 end
 
-function set_design!(themesym::Symbol; font="Palatino", dpi=300, tickfontsize=12, kwargs...)
+function set_design!(themesym::Symbol; font="Palatino", dpi=300, tickfontsize=12, plot=())
     PlotEnvironment.design = themesym
     theme(themesym)
-    default(fontfamily=font, dpi=dpi, tickfontsize=tickfontsize,kwargs...)
+    default(fontfamily=font, dpi=dpi, tickfontsize=tickfontsize,plot...)
 end
 
-function set_design!(plotter::PlotEnv, themesym::Symbol; font="Palatino", dpi=300, tickfontsize=12, kwargs...)
+function set_design!(plotter::PlotEnv, themesym::Symbol; font="Palatino", dpi=300, tickfontsize=12, plot=())
     plotter.design = themesym
     theme(themesym)
-    default(fontfamily=font, dpi=dpi, tickfontsize=tickfontsize,kwargs...)
+    default(fontfamily=font, dpi=dpi, tickfontsize=tickfontsize,plot...)
 end
